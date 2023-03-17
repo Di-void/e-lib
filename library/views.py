db9f9305cbe735240fc6b37ff73ea474a0c90de1
DEE <donzydonathan@gmail.com>
2023-03-18 00:54:50 +0100

    feat: complete setting up sign up and sign in pages with dynamic rendering

--- library/views.py
+++ library/views.py
@@ -1,7 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from django.views import View
+from .forms import SignUpForm
 
 # Create your views here.
 
 def index(request):
     """View Function for the Landing Page of the Site"""
     return render(request, 'index.html')
+
+# def signup(request):
+#     if request.method == "POST":
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data.get('username')
+#             raw_password = form.cleaned_data.get('password1')
+#             user = authenticate(username=username, password=raw_password)
+#             login(request, user)
+#             return redirect('home')
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'sign_up.html')
+
+class RegisterView(View):
+    form_class = SignUpForm
+    initial = {'key': 'value'}
+    template_name = 'sign_up.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect(to='/')
+
+        return super(RegisterView, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data.get('username')
+
+            return redirect(to='login')
+
+        return render(request, self.template_name, {'form': form})
