a378045b29432e92ba114e319b88b2dbfb1d0ad5
DEE <donzydonathan@gmail.com>
2023-03-22 22:18:47 +0100

    feat: implement videos library page and fix styling issues

--- videos/views.py
+++ videos/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
+@login_required
 def index(request):
-    return HttpResponse("Hello from Videos Application")
+    return render(request, 'videos/index.html')
