07b18cd0ea568b8a9dc5854b373866f1676ed962
DEE <donzydonathan@gmail.com>
2023-03-26 01:32:45 +0100

    feat: complete book filter functionality based on categories in ebook library

--- books/views.py
+++ books/views.py
@@ -6,10 +6,35 @@ from library.models import Book, Category
 
 @login_required
 def index(request):
-    books = Book.objects.all()
+    params = request.GET
+    if (params):
+        category = params['category']
+    else:
+        category = 'all'
+    
+    def formatted_category(item):
+        label = item
+        value = (str(item)).lower()
+        if ('&' in value):
+            value = value.replace('&', '%26')
+
+        return (label, value)
+        
+    if( category == 'all' ):
+        books = Book.objects.all()
+    else:
+        books = Book.objects.all().filter(category__name__iexact=category)
+        print(books)
+        
+
+    result = Category.objects.all()    
+    categories = list(map(formatted_category, result))
+    
 
     context = {
-        'books': books
+        'books': books,
+        'categories': categories,
+        'category': category
     }
 
     return render(request, 'books/index.html', context=context)
