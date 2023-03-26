a2201589cca778d8afeb4e7b557ef55d7f1c15a3
DEE <donzydonathan@gmail.com>
2023-03-26 21:56:12 +0100

    feat: dynamically render videos from DB in Videos Library page

--- videos/views.py
+++ videos/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from library.models import Category, Video
 
 # Create your views here.
 
 @login_required
 def index(request):
-    return render(request, 'videos/index.html')
+    params = request.GET
+    if (params):
+        category = params['category']
+    else:
+        category = 'all'
+    
+    def formatted_category(item):
+        label = item
+        value = (str(item)).lower()
+        if ('&' in value):
+            value = value.replace('&', '%26')
+
+        return (label, value)
+
+    if (category == 'all'):
+        videos = Video.objects.all()
+    else:
+        videos = Video.objects.all().filter(category__name__iexact=category)
+
+    result = Category.objects.all()
+    categories = list(map(formatted_category, result))
+
+    context = {
+        'videos': videos,
+        'categories': categories,
+        'category': category
+    }    
+
+    return render(request, 'videos/index.html', context=context)
