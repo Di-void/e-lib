2530b0e3caa9234222ea4d25643c828be073e0ee
DEE <donzydonathan@gmail.com>
2023-03-27 14:10:04 +0100

    feat: complete books and video search functionality

--- videos/views.py
+++ videos/views.py
@@ -2,10 +2,22 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from library.models import Category, Video
 
+from books.forms import SearchForm
+
 # Create your views here.
 
 @login_required
 def index(request):
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+
+        if form.is_valid():
+            searchKey = form.cleaned_data['title']
+            videos = Video.objects.filter(title__contains=searchKey)
+            return render(request, 'videos/index.html', {'form': form, 'videos': videos})
+        else:
+            form = SearchForm()
+    form = SearchForm()
     params = request.GET
     if (params):
         category = params['category']
@@ -23,7 +35,7 @@ def index(request):
     if (category == 'all'):
         videos = Video.objects.all()
     else:
-        videos = Video.objects.all().filter(category__name__iexact=category)
+        videos = Video.objects.filter(category__name__iexact=category)
 
     result = Category.objects.all()
     categories = list(map(formatted_category, result))
@@ -31,7 +43,8 @@ def index(request):
     context = {
         'videos': videos,
         'categories': categories,
-        'category': category
+        'category': category,
+        'form': form
     }    
 
     return render(request, 'videos/index.html', context=context)
